5e5b151b054564b9a02a5fbcde120493e5787795
Hector <hector141599@gmail.com>
2022-06-15 08:14:48 -0500

    Inciando encabezados de Home

--- view/screens/HomeScreen.js
+++ view/screens/HomeScreen.js
@@ -1,11 +1,22 @@
 import React from "react";
-import {View,Text} from "react-native";
+import {View,Text, SafeAreaView} from "react-native";
+import COLORS from "../../consts/colors";
 const HomeScreen=()=>{
     return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
+        <SafeAreaView style={{flex:1,
+        paddingHorizontal:20,
+        backgroundColor:COLORS.white}}>
+            <View style={style.header}></View>
+
+        </SafeAreaView>
     )
 }
 
+const style=StyleSheet.create({
+    header:{
+        marginTop:20,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    }
+})
 export default HomeScreen;
