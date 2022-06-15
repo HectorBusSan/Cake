a8f9145f3474cd68311feeff7dea082b3b4578db
Hector <hector141599@gmail.com>
2022-06-15 09:26:54 -0500

    Agregando input e iconos

--- view/screens/HomeScreen.js
+++ view/screens/HomeScreen.js
@@ -1,18 +1,29 @@
 import React from "react";
 import {View,Text, SafeAreaView, StyleSheet} from "react-native";
 import COLORS from "../../consts/colors";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
+import { TextInput } from "react-native-gesture-handler";
 
 const HomeScreen=()=>{
     return (
         <SafeAreaView style={{flex:1,
         paddingHorizontal:20,
         backgroundColor:COLORS.white}}>
-            <View style={style.header}></View>
+            <View style={style.header}>
                 <View>
                     <Text style={{fontSize:25,fontWeight:"bold"}}>Welcome to </Text>
                     <Text style={{fontSize:38,fontWeight:"bold",color:COLORS.green}}>Cakes Shop</Text>
-                    <FontAwesome name="shopping-cart" size={24}/>
+                </View>
+                <FontAwesome name="shopping-cart" size={24}/>
+            </View>
+                <View style={{marginTop:30, flexDirection:"row", alignItems:"center"}}>
+                    <View style={style.searchContainer}>
+                        <FontAwesome name="search" size={19} style={{marginLeft:20}}/>
+                        <TextInput placeholder=" Search..." style={style.input} />
+                    </View>
+                    <View style={style.sortBtn}>
+                        <MaterialIcons name="sort" size={30} color={COLORS.white}/>
+                    </View>
                 </View>
         </SafeAreaView>
     )
@@ -20,10 +31,33 @@ const HomeScreen=()=>{
 
 const style=StyleSheet.create({
     header:{
-        marginTop:20,
+        marginTop:10,
         flexDirection:"row",
         justifyContent:"space-between",
-
+    },
+    searchContainer:{
+        height:50,
+        backgroundColor:COLORS.light,
+        borderRadius:10,
+        flex:1,
+        flexDirection:"row",
+        alignItems:"center"
+    },
+    input:{
+        fontSize:18,
+        fontWeight:"bold",
+        color:COLORS.dark,
+        flex:1,
+        marginLeft:10
+    },
+    sortBtn:{
+        margin:10,
+        height:50,
+        width:50,
+        backgroundColor:COLORS.green,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius:10
     }
 })
 export default HomeScreen;
