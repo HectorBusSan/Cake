e1ba1eff27cd815f0cb98ed3bd1c784d56b29e25
Hector <hector141599@gmail.com>
2022-06-15 09:01:04 -0500

    Agregando primer Icono con expo

--- view/screens/HomeScreen.js
+++ view/screens/HomeScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
-import {View,Text, SafeAreaView} from "react-native";
+import {View,Text, SafeAreaView, StyleSheet} from "react-native";
 import COLORS from "../../consts/colors";
+import { FontAwesome } from "@expo/vector-icons";
+
 const HomeScreen=()=>{
     return (
         <SafeAreaView style={{flex:1,
         paddingHorizontal:20,
         backgroundColor:COLORS.white}}>
             <View style={style.header}></View>
-
+                <View>
+                    <Text style={{fontSize:25,fontWeight:"bold"}}>Welcome to </Text>
+                    <Text style={{fontSize:38,fontWeight:"bold",color:COLORS.green}}>Cakes Shop</Text>
+                    <FontAwesome name="shopping-cart" size={24}/>
+                </View>
         </SafeAreaView>
     )
 }
@@ -16,7 +22,8 @@ const style=StyleSheet.create({
     header:{
         marginTop:20,
         flexDirection:"row",
-        justifyContent:"space-between"
+        justifyContent:"space-between",
+
     }
 })
 export default HomeScreen;
