a94a1362c61a3bbf817d2fceb8869bf81cbb1271
Hector <hector141599@gmail.com>
2022-06-15 20:44:26 -0500

    Exportamos nuestros productos

--- consts/products.js
+++ consts/products.js
@@ -11,7 +11,7 @@ const products=[
         id:2,
         name:"Dragon Plant",
         price:'25.99',
-        like:true,
+        like:false,
         img: require('./../assets/example2.png'),
         about:'Dragon Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.'
     },
@@ -34,15 +34,16 @@ const products=[
         id:5,
         name:"Ravenea Plant",
         price:'39.99',
-        like:true,
+        like:false,
         img: require('./../assets/example5.png'),
         about:'Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.'
     },{
         id:5,
         name:"Dragon Plant",
         price:'59.99',
-        like:true,
+        like:false,
         img: require('./../assets/example6.png'),
         about:'Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.'        
     }
 ]
+export default products;
