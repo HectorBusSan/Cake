070e5283a0ecbcd73546baf8552a47050870f3c9
Hector <hector141599@gmail.com>
2022-06-16 08:36:38 -0500

    Agregando Icono y Regreso a Details

--- view/screens/DetailsScreen.js
+++ view/screens/DetailsScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
-import {View,Text}from "react-native";
+import {View,Text,StyleSheet, SafeAreaView}from "react-native";
+import COLORS from "../../consts/colors";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
-const DetailsScreen=()=>{
+const DetailsScreen=({navigation,route})=>{
+    const product = route.params;
+    console.log(product)
     return(
-        <View>
-            <Text>DETAILSSCREEN</Text>
-        </View>
+        <SafeAreaView style={{flex:1,backgroundColor:COLORS.white}}>
+            <View style={style.header}>
+                <MaterialIcons name="arrow-back-ios" size={24} color="black" onPress={()=>{navigation.goBack()}}/>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const style = StyleSheet.create({
+    header:{paddingHorizontal:20, marginTop:20,flexDirection:"row",justifyContent:"space-between"}
+})
 export default DetailsScreen;
