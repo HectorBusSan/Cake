502ed2d489483ff2b337928c1fdf42a435a9434a
Hector <hector141599@gmail.com>
2022-06-16 21:26:10 -0500

    Creando parte superior de Details

--- view/screens/DetailsScreen.js
+++ view/screens/DetailsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View,Text,StyleSheet, SafeAreaView}from "react-native";
+import {View,Text,StyleSheet, SafeAreaView, Image}from "react-native";
 import COLORS from "../../consts/colors";
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
@@ -10,12 +10,48 @@ const DetailsScreen=({navigation,route})=>{
         <SafeAreaView style={{flex:1,backgroundColor:COLORS.white}}>
             <View style={style.header}>
                 <MaterialIcons name="arrow-back-ios" size={24} color="black" onPress={()=>{navigation.goBack()}}/>
+                <FontAwesome name="shopping-cart" size={24}/>
+            </View>
+            <View style={style.imageContainer}>
+                <Image source={product.img} style={{resizeMode:"contain",flex:1,}}/>
+            </View>
+            <View style={style.detailsContainer}>
+                <View style={{marginLeft:20,flexDirection:"row",alignItems:"flex-end"}}>
+                    <View style={style.line}/>
+                    <Text>Best Choice</Text>
+                </View>
             </View>
         </SafeAreaView>
     )
 }
 
 const style = StyleSheet.create({
-    header:{paddingHorizontal:20, marginTop:20,flexDirection:"row",justifyContent:"space-between"}
+    header:{paddingHorizontal:20,
+        marginTop:20,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    },
+    imageContainer:{
+        flex:0.45,
+        marginTop:20,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    detailsContainer:{
+        flex:0.55,
+        backgroundColor:COLORS.light,
+        marginHorizontal:7,
+        marginBottom:7,
+        marginTop:30,
+        paddingTop:30,
+        borderRadius:40
+    },
+    line:{
+        width:25,
+        height:2,
+        backgroundColor:COLORS.dark,
+        marginBottom:5,
+        marginRight:3
+    }
 })
 export default DetailsScreen;
