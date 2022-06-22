22c1defa6c7ee9a83c4f9ee118eedce112829f0e
Hector <hector141599@gmail.com>
2022-06-21 20:04:24 -0500

    Agregando boton de Login al onBoarding

--- view/screens/Onboarding.js
+++ view/screens/Onboarding.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {View,Text, ImageBackground, StyleSheet} from "react-native"
+import { TouchableOpacity } from "react-native-gesture-handler";
 import COLORS from "../../consts/colors";
-const Onboarding=()=>{
+
+const Onboarding=({navigation})=>{
     return(
         <View style={{flex:1}}>
             <StatusBar translucent backgroundColor="rgba(0,0,0,0)"/>
@@ -13,9 +15,12 @@ const Onboarding=()=>{
                     <Text style={{color:COLORS.white,fontSize:32,fontWeight:"bold"}}>Cake Shop</Text>
                     <Text style={{color:COLORS.white,fontSize:31,fontWeight:"bold"}}>Fine Bakery</Text>
                     <Text style={{color:COLORS.white,lineHeight:25,marginTop:15}}>The Best Cake shop of the whole world, enjoy us desserts and cakes</Text>
+                    <TouchableOpacity activeOpacity={0.8}
+                    onPress={()=>navigation.navigate("Home")}>
                     <View style={style.btn}>
                         <Text style={{fontWeight:"bold"}}>Login</Text>
                     </View>
+                    </TouchableOpacity>
                 </View>
             </ImageBackground>
         </View>
