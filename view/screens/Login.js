5b0e13770055cba8d3b117d39462156cfd0010e6
Hector <hector141599@gmail.com>
2022-06-21 21:37:32 -0500

    Agregando Return a Login y Register

---
+++ view/screens/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet,Text,View } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
+const Login=({navigation})=>{
+    return(
+        
+    )
+}
+export default Login;
---
+++ view/screens/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet,Text,View } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
+const Register=()=>{
+    return(
+        <Text>Hola Register</Text>
+    )
+}
+export default Register;
