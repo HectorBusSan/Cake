61c2aacf456716ef9b6cadf91325d07695e40ecb
Hector <hector141599@gmail.com>
2022-06-21 21:48:55 -0500

    Agregando el TouchableOpacity a Register

--- view/screens/Register.js
+++ view/screens/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet,Text,View } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Register=()=>{
     return(
