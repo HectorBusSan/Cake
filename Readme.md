1f5bc0e392aaee0444b6afa60c79b0db6501a8dd
Hector <hector141599@gmail.com>
2022-06-22 11:31:15 -0500

    Agregando animaciones a los botones

--- Readme.md
+++ Readme.md
@@ -5,3 +5,4 @@ El sistema tiene tecnologia de React Native, Expo.
 - Ya teniendo inicado tanto git y expo pasaremos instalar dependecnias como lo que son:
 - npm install [@react-navigation/stack](https://reactnavigation.org/docs/stack-navigator/).
 - Agregar dependecias de Iconos react-native-vector-icons o el por defecto de EXPO
+- Agregando dev para animaciones npm i react-native-reanimated
--- view/screens/Login.js
+++ view/screens/Login.js
@@ -1,29 +1,30 @@
-import React,{useState} from "react";
+import React,{useRef, useState} from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet,Text,View, ImageBackground, Dimensions } from "react-native";
+import { StyleSheet,Text,View, ImageBackground, Dimensions,Animated } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import COLORS from "../../consts/colors";
-import { Animated } from "react-native";
-import { TapGestureHandler } from "react-native-gesture-handler";
+
 
 const {height,width}=Dimensions.get("window");
 
 const Login=({navigation})=>{
 
-    const [opacity, setOpacity] = useState(1);
-    const changer=()=>{
-        setOpacity(0)
-        console.log("opacidad")
+const fadeAnim= useRef(new Animated.Value(1)).current;
+
+    const fadeIn=()=>{
+        Animated.timing(fadeAnim,{
+            toValue:1,
+            duration:5000
+        }).start({finished: false})
     }
 
-    function runTiming(clock, value, dest){
-        const state={
-            finished: new Value(0),
-            position: new Value(0),
-            time: new Value(0),
-            fameTime:new Value(0)
-        }
+    const fadeOut=()=>{
+        Animated.timing(fadeAnim,{
+            toValue:0,
+            duration:3000,
+        }).start()
     }
+
 
     return(
         <View style={{flex:1, backgroundColor:COLORS.white}}>
@@ -32,14 +33,16 @@ const Login=({navigation})=>{
                 style={{flex:1,height:null,width:null,justifyContent:"flex-end"}}>
                 
                 <View style={{height:height/3}}>
-                    <TouchableOpacity onPress={changer}>
-                        <Animated.View style={{...styles.button,opacity:opacity}}>
+                    <TouchableOpacity onPress={fadeOut}>
+                        <Animated.View style={{...styles.button,opacity:fadeAnim}}>
                                 <Text style={{fontSize:20,fontWeight:"bold"}}>Sing In</Text>
                         </Animated.View>
                     </TouchableOpacity>
-                    <View style={{...styles.button,marginTop:20, backgroundColor:"#2E71DC"}}>
-                        <Text style={{fontSize:20,fontWeight:"bold", color:COLORS.white}}>Register</Text>
-                    </View>
+                    <TouchableOpacity onPress={fadeIn}>
+                        <View style={{...styles.button,marginTop:20, backgroundColor:"#2E71DC"}}>
+                            <Text style={{fontSize:20,fontWeight:"bold", color:COLORS.white}}>Register</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
                 </ImageBackground>
         </View>
