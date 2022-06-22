c3824dba059159778d71f64d3465733b6fb99e50
Hector <hector141599@gmail.com>
2022-06-21 21:48:23 -0500

    Inciando con el background de login

--- view/screens/Login.js
+++ view/screens/Login.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { StyleSheet,Text,View } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet,Text,View, ImageBackground } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import COLORS from "../../consts/colors";
 
 const Login=({navigation})=>{
     return(
-        
+        <View style={{flex:1, backgroundColor:COLORS.white}}>
+            <StatusBar translucent backgroundColor="rgba(0,0,0,0)"/>
+            
+                <ImageBackground source={require("./../../assets/LoginBack.jpeg")}
+                style={{flex:1,height:null,width:null}}>
+
+                </ImageBackground>
+            
+        </View>
     )
 }
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    }
+})
+
 export default Login;
