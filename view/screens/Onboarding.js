9f50f3381511768f9e877e5d06246047bdd7a6ed
Hector <hector141599@gmail.com>
2022-06-21 19:37:42 -0500

    Agregando Texto a Onboarding

--- view/screens/Onboarding.js
+++ view/screens/Onboarding.js
@@ -1,26 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {View,Text, ImageBackground, StyleSheet} from "react-native"
+import COLORS from "../../consts/colors";
 const Onboarding=()=>{
     return(
         <View style={{flex:1}}>
             <StatusBar translucent backgroundColor="rgba(0,0,0,0)"/>
             <ImageBackground
             style={{flex:1}}
-            source={require("./../../assets/onboardingback.jpeg")}></ImageBackground>
-            <View style={style.description}>
-                
-            </View>
+            source={require("./../../assets/onboardingback.jpeg")}>
+                <View style={style.description}>
+                    <Text style={{color:COLORS.white,fontSize:32,fontWeight:"bold"}}>Cake Shop</Text>
+                    <Text style={{color:COLORS.white,fontSize:31,fontWeight:"bold"}}>Fine Bakery</Text>
+                    <Text style={{color:COLORS.white,lineHeight:25,marginTop:15}}>The Best Cake shop of the whole world, enjoy us desserts and cakes</Text>
+                </View>
+            </ImageBackground>
         </View>
     )
 }
 
 const style= StyleSheet.create({
     description:{
-    heigth:"50%",
+    height:"50%",
     bottom:0,
-    position:"abosolute",
-    paddingHorizontal:40
+    position:"absolute",
+    paddingHorizontal:40,
     }
 })
 
