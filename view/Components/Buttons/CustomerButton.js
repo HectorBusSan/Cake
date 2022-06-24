8cba941312111fd0dcd96d6f25f70f5dbab92b8b
Hector <hector141599@gmail.com>
2022-06-24 07:08:39 -0500

    Modificnado Buttons para distintos tipos

--- view/Components/Buttons/CustomerButton.js
+++ view/Components/Buttons/CustomerButton.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import { View,Text, StyleSheet, Pressable } from 'react-native';
+import COLORS from '../../../consts/colors';
 
-export const CustomerButton = ({onPress, text}) => {
+export const CustomerButton = ({onPress, text, type="PRYMARY"}) => {
   return (
-    <Pressable onPress={onPress} style={styles.container}>
-      <Text style={styles.text}>{text}</Text>
+    <Pressable onPress={onPress} style={[styles.container,styles[`container_${type}`]]}>
+      <Text style={[styles.text,styles[`text_${type}`]]}>{text}</Text>
     </Pressable>
   )
 }
 
 const styles=StyleSheet.create({
   container:{
-    backgroundColor:"#3B71F3",
     width:"100%",
     padding:15,
     marginVertical:5,
     alignItems:"center",
     borderRadius:30
   },
+  container_PRYMARY:{
+    backgroundColor:"#3B71F3",
+  },
+  container_TETIARY:{
+    borderColor:"#e8e8e8"
+  },
   text:{
     fontWeight:"bold",
     color:"#fff"
+  },text_TETIARY:{
+    color:"gray"
+  },container_SECOND:{
+    backgroundColor:COLORS.green
+  },text_SECOND:{
+    color:"#FFF"
   }
 })
 
