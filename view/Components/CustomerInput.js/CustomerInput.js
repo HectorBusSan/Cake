766fed45f763ababff1baf7db796f37f4f462c87
Hector <hector141599@gmail.com>
2022-06-24 06:23:42 -0500

    Actualizando Login

--- view/Components/CustomerInput.js/CustomerInput.js
+++ view/Components/CustomerInput.js/CustomerInput.js
@@ -1,16 +1,28 @@
 import { View, Text, TextInput, StyleSheet} from 'react-native'
 import React from 'react'
 
-const CustomerInput = () => {
+const CustomerInput = ({value,setValue,Placeholder,secureTextEntry}) => {
   return (
-    <View style={style.constainer}>
-      <TextInput placeholder='placeholder' style={style.input}/>
+    <View style={style.container}>
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        placeholder={Placeholder}
+        style={style.input}
+        secureTextEntry={secureTextEntry}/>
     </View>
   )
 }
 const style= StyleSheet.create({
     container:{
-
+        backgroundColor:"#fff",
+        width:"100%",
+        borderColor:"#e8e8e8",
+        borderWidth:1,
+        borderRadius:5,
+        padding:10,
+        marginHorizontal:10,
+        marginVertical:5
     },input:{
         
     }
