023396adda0fc75e3884af574ae844db2458b1a1
Hector <hector141599@gmail.com>
2022-06-30 21:40:23 -0500

    Ocultando Cors un momento

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app= express();
 app.use(express.json());
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const {PrismaClient}= require("@prisma/client");
 const prisma = new PrismaClient();
 
-const cors = require("cors");
-const corsOptions={
-    origin:"http://localhost:3000"
-}
+// const cors = require("cors");
+// const corsOptions={
+//     origin:"http://localhost:3000"
+// }
 app.use(cors(corsOptions));
 
 app.get("/",async(req,res)=>{
