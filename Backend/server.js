375aa6d858f061efbfad00dc3fb1c560aa14e679
Hector <hector141599@gmail.com>
2022-06-30 21:22:38 -0500

    Creando servidor

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app= express();
+app.use(express.json());
+const port = process.env.PORT || 3001;
+
+const {PrismaClient}= require("@prisma/client");
+const prisma = new PrismaClient();
+
+const cors = require("cors");
+const corsOptions={
+    origin:"http://localhost:3000"
+}
+app.use(cors(corsOptions));
+
+app.get("/",async(req,res)=>{
+    res.json({message:"Here"});
+});
+app.listen(port,()=>{
+    console.log(`Listening Port: ${port}`)
+});
+
