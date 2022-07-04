ebc88912042d76b82c79b0a3380448d8505ea6f2
Hector <hector141599@gmail.com>
2022-07-04 07:20:42 -0500

    Crenado envio de datos por POST

---
+++ api.js
@@ -0,0 +1,10 @@
+const API= "http://10.0.2.2:3000/usuarios";
+
+export const saveUser=async(user)=>{
+    const res= await fetch(API,{
+        method:"POST",
+        headers:{Accept:"application/json","Content-type":"application/json"},
+        body:JSON.stringify(user)
+    });
+    return await res.json(res);
+}
