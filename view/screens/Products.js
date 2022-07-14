fc37063ae0e5de9d504af4bf848ca55212eb0ab7
Hector <hector141599@gmail.com>
2022-07-13 21:22:57 -0500

    Agregando sección de crear productos

---
+++ view/screens/Products.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Products = () => {
+  return (   
+    <View>
+      <Text>Products</Text>
+    </View>
+  )
+}
+
+export default Products
