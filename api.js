b426e14c6d81a968820dcf22f3c0e6550107daff
Hector <hector141599@gmail.com>
2022-07-22 20:36:12 -0500

    Solictando que por metodo POST enviar la data

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const API= "http://10.0.2.2:3000/usuarios";
+const API2= "http://10.0.2.2:3000/productos"
 
 export const saveUser=async(user)=>{
     const res= await fetch(API,{
@@ -13,6 +14,14 @@ export const getUser=async(log)=>{
         method:"POST",
         headers:{Accept:"application/json","Content-type":"application/json"},
         body:JSON.stringify(log)
+    });
+    return await res.json();
+}
+export const saveProduct= async(pr)=>{
+    const res= await fetch(API2,{
+        method:"POST",
+        headers:{Accept:"application/json","Content-type":"application/json"},
+        body:JSON.stringify(pr)
     });
     return await res.json();
 }
