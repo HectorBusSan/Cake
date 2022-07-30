a5ea0082755e040829622169fee4ac7cfa7ed034
Hector <hector141599@gmail.com>
2022-07-29 19:23:13 -0500

    Agregando solicitud al backend de crear producto POST

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const API= "http://10.0.2.2:3000/usuarios";
 const API2= "http://10.0.2.2:3000/productos"
+const API3= "http://10.0.2.2:3000/upload";
+
 
 export const saveUser=async(user)=>{
     const res= await fetch(API,{
@@ -25,3 +27,29 @@ export const saveProduct= async(pr)=>{
     });
     return await res.json();
 }
+export const uploadI= async(img)=>{
+    const res=await fetch(API3,{
+        method:"POST",
+        headers:{Accept:"application/json","Content-type":"application/json"},
+        body:JSON.stringify(img)
+    })
+    return await res.json();
+}
+
+// export const addImageToListing= async(listingId,image)=>{
+//     const payload= new FormData();
+//     payload.append("image",image);
+
+//     if(image.isCoverImage){
+//         payload.append("isCoverImage",true);
+//     }
+//     const config={
+//         body:payload,
+//         method:"POST",
+//         headers:{
+//             "Content-Type":"multipart/form-data",
+//         },
+//     };
+//     const response = await request(`listings/${listingId}/image`,config);
+//     return response;
+// }
