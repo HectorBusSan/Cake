979c6f38d176bad049dbb5e26e1ce0dddfcd8d5c
Hector <hector141599@gmail.com>
2022-08-02 22:20:28 -0500

    Recibir datos

--- view/screens/MyCart.js
+++ view/screens/MyCart.js
@@ -2,36 +2,38 @@ import { View, Text,StyleSheet,SafeAreaView } from 'react-native'
 import React,{useState,useEffect} from 'react'
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage"
-import Productos from './Productos';
+import products from '../../consts/products';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 const MyCart = ({navigation}) => {
     const [product, setProduct] = useState()
     const [total, setTotal] = useState(null)
     useEffect(() => {
-      const unsubscribe= navigation.addListener("focus",()=>{
-        getDataFromDB();
-      })
+    //   const unsubscribe= navigation.addListener("focus",()=>{
+    //     getDataFromDB();
+    //   })
+    getDataFromDB();
     }, [navigation])
     
     const getDataFromDB=async()=>{
-        // let items=await AsyncStorage.getItem("cartItem")
-        // items = JSON.parse(items);
-        // let productData=[];
-        // if(items){
-        //     Productos.forEach(data=>{
-        //         if(items.includes(data.id)){
-        //             productData.push(data)
-        //             return
-        //         }
-        //     })
-        //     setProduct(productData)
-        //     getTotal(productData)
-        // }
-        // else{
-        //     setProduct(false)
-        //     getTotal(false)
-        // }
-        const resp=await AsyncStorage.getItem("cartItem");
-        setProduct(resp)
+        let items=await AsyncStorage.getItem("cartItem")
+        items = JSON.parse(items);
+        let productData=[];
+        if(items){
+            products.forEach(data=>{
+                if(items.includes(String(data.id))){
+                    productData.push(data)
+                    return;
+                }
+            })
+            setProduct(productData)
+            getTotal(productData) 
+        }
+        else{
+            setProduct(false)
+            getTotal(false)
+        }
+        // // const resp=await AsyncStorage.getItem("cartItem");
+        // // setProduct(resp)
     }
 const getTotal=(productData)=>{
     let total=0;
@@ -47,25 +49,25 @@ const getTotal=(productData)=>{
         <View style={style.header}>
             <MaterialIcons name="arrow-back-ios" size={24} color="black" onPress={()=>{navigation.goBack()}}/>
             <View style={{display:"flex", flexDirection:"row", width:"20%", justifyContent:"space-between"}}>
-                <FontAwesome name="user-circle-o" size={24} color="black" />
+                <TouchableOpacity onPress={()=>console.log(product)}><FontAwesome name="user-circle-o" size={24} color="black" /></TouchableOpacity>
                 <FontAwesome name="shopping-cart" size={24} />
             </View>
         </View>
         <View>
-            {/* {
+            {
                 product?
                 product.map((data,index)=>{
                     return(
-                        <Text>{data}</Text>
+                        <Text>{data.name}</Text>
                     )
                 })
-                :<Text>NO hay</Text>
-            } */}
-            {
+                :null
+            }
+            {/* {
                 product?
                 <Text>{product}</Text>:
                 <Text>NO hay</Text>
-            }
+            } */}
         </View>
     </SafeAreaView>
   )
