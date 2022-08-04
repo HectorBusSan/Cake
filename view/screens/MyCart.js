8f3a7bacf6f09e5084eb4954394945505074716f
Hector <hector141599@gmail.com>
2022-08-03 20:27:35 -0500

    Agregando contendio al carrito

--- view/screens/MyCart.js
+++ view/screens/MyCart.js
@@ -1,9 +1,10 @@
-import { View, Text,StyleSheet,SafeAreaView } from 'react-native'
+import { View, Text,StyleSheet,SafeAreaView, ScrollView,Image } from 'react-native'
 import React,{useState,useEffect} from 'react'
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import products from '../../consts/products';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import COLORS from '../../consts/colors';
 const MyCart = ({navigation}) => {
     const [product, setProduct] = useState()
     const [total, setTotal] = useState(null)
@@ -44,8 +45,40 @@ const getTotal=(productData)=>{
     setTotal(total);
 
 }
+const renderProduct =(data,index)=>{
+    return(
+        <TouchableOpacity style={{width:"100%",
+         height:100,marginVertical:6,flexDirection:"row"}}>
+            <View style={{
+                width:"30%",height:100,
+                padding:12,flexDirection:"row",
+                alignItems:"center",justifyContent:"center",
+                backgroundColor:COLORS.light,
+                borderRadius:10,
+                marginRight:22
+            }}>
+                <Image source={data.img}
+                style={{width:"100%",height:"100%",resizeMode:"contain"}}/>
+            </View>
+            <View style={{
+                flex:1,
+                height:"100%",
+                justifyContent:"space-around"
+            }}>
+                <View>
+                    <Text style={{fontSize:14,maxWidth:"100%",color:COLORS.dark,
+                    fontWeight:"600",letterSpacing:1}}>{data.name}</Text>
+                    <View style={{marginTop:5}}>
+                        <Text style={{}}>${data.price}</Text>
+                    </View>
+
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor:"#fffccc"}}>
         <View style={style.header}>
             <MaterialIcons name="arrow-back-ios" size={24} color="black" onPress={()=>{navigation.goBack()}}/>
             <View style={{display:"flex", flexDirection:"row", width:"20%", justifyContent:"space-between"}}>
@@ -53,21 +86,27 @@ const getTotal=(productData)=>{
                 <FontAwesome name="shopping-cart" size={24} />
             </View>
         </View>
-        <View>
-            {
-                product?
-                product.map((data,index)=>{
-                    return(
-                        <Text>{data.name}</Text>
-                    )
-                })
-                :null
-            }
-            {/* {
-                product?
-                <Text>{product}</Text>:
-                <Text>NO hay</Text>
-            } */}
+        <View style={{
+            width:"100%",
+            height:"100%",
+            backgroundColor:COLORS.white,
+            padding:20,
+            paddingTop:10,
+            marginTop:15,
+            borderRadius:25
+        }}>
+            <ScrollView>
+                <View>
+                    <Text style={{fontSize:20,
+                    color:COLORS.dark,
+                    fontWeight:"400",
+                    letterSpacing:1,
+                    paddingTop:2, marginBottom:10}}>My Cart</Text>
+                    <View>
+                        {product?product.map(renderProduct):null}
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     </SafeAreaView>
   )
@@ -79,6 +118,6 @@ const style = StyleSheet.create({
     header:{paddingHorizontal:20,
         marginTop:50,
         flexDirection:"row",
-        justifyContent:"space-between"
+        justifyContent:"space-between",
     },
 });
