6ab75c86e2a24f473ca5fca2822eabcd25aa7d05
Hector <hector141599@gmail.com>
2022-08-08 17:00:40 -0500

    Envio de pedido desde api

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const API= "http://10.0.2.2:3000/usuarios";
 const API2= "http://10.0.2.2:3000/productos"
 const API3= "http://10.0.2.2:3000/upload";
+const API4= "http://10.0.2.2:3000/pedidos";
 
 
 export const saveUser=async(user)=>{
@@ -53,3 +54,12 @@ export const uploadI= async(img)=>{
 //     const response = await request(`listings/${listingId}/image`,config);
 //     return response;
 // }
+
+export const sendOrder=async(order)=>{
+    const res=await fetch(API4,{
+        method:"POST",
+        headers:{Accept:"application/json","Content-type":"application/json"},
+        body:JSON.stringify(order)
+    })
+    return await res.json();
+}
