289cd811b9531573658bb4192e6d25e491d0dccd
Hector <hector141599@gmail.com>
2022-08-07 20:11:49 -0500

    Agregand actualización de celdas de tabla de pedidos

--- prisma/migrations/20220808010547_init/migration.sql
+++ prisma/migrations/20220808010547_init/migration.sql
@@ -2,11 +2,14 @@
 CREATE TABLE `pedido` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `codigop` VARCHAR(250) NOT NULL,
+    `codcake` VARCHAR(250) NOT NULL,
     `idproducto` INTEGER NOT NULL,
     `cantidad` INTEGER NOT NULL,
     `total` DOUBLE NOT NULL,
     `fechai` DATETIME(3) NOT NULL,
     `fechaf` DATETIME(3) NOT NULL,
+    `username` VARCHAR(250) NOT NULL,
 
+    UNIQUE INDEX `pedido_codcake_key`(`codcake`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
