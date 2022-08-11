47e1768a674a00b45f70696f50b975815c1549c1
Hector <hector141599@gmail.com>
2022-08-10 23:32:01 -0500

    Enviando solicitud al servidor de pedidos del usuario

--- view/screens/Pedidos.js
+++ view/screens/Pedidos.js
@@ -1,9 +1,22 @@
 import { View, Text, SafeAreaView, StyleSheet,TouchableOpacity } from 'react-native'
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { MaterialIcons,FontAwesome } from '@expo/vector-icons'
-
+import { orders } from '../../api'
 const Pedidos = ({navigation,route}) => {
     const Usuario=route.params;
+    const [pedidos, setPedidos] = useState()
+    useEffect(() => {
+      return () => {
+        (async()=>{
+            const task= await orders(Usuario.username);
+            // console.log(task)
+            setPedidos([task]);
+            
+        })();
+        console.log(pedidos);
+      }
+    }, [])
+    
   return (
     <SafeAreaView>
         <View style={style.header}>
@@ -12,7 +25,10 @@ const Pedidos = ({navigation,route}) => {
         <View style={{marginVertical:10,paddingHorizontal:20}}>
             <Text style={{fontSize:24}}>Pedidos: {Usuario.username}</Text>
             <View style={{width:"100%",backgroundColor:"#ccc",height:"100%",borderRadius:20,marginTop:20,padding:10}}>
-                <Text>Podras consultar tus pedidos en esta Pantalla</Text>
+                <Text>Podras consultar tus pedidos en esta Pantalla:</Text>
+            </View>
+            <View>
+                <Text>{pedidos>1?pedidos:null}</Text>
             </View>
         </View>
     </SafeAreaView>
