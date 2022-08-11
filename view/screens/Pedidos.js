526ff239cc17b0b9e4af0384592a6742278a29dc
Hector <hector141599@gmail.com>
2022-08-10 22:07:04 -0500

    Creando interfaz de Pedidos

---
+++ view/screens/Pedidos.js
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Pedidos = ({navigation}) => {
+  return (
+    <SafeAreaView>
+
+        <View>
+            <Text>Pedidos</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default Pedidos
