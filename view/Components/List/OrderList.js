e00754cd40593fe14cfe119afb86cfb44e86f314
Hector <hector141599@gmail.com>
2022-08-11 18:52:05 -0500

    Agregando a OrderList el OrderItem

--- view/Components/List/OrderList.js
+++ view/Components/List/OrderList.js
@@ -1,10 +1,33 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import OrderItem from './OrderItem'
+import { FlatList } from 'react-native-gesture-handler'
 
 const OrderList = () => {
+    const [pedidos, setPedidos] = useState([])
+
+    const loadOrders=async()=>{
+        const data= await orders();
+        setPedidos(data);
+    }
+    useEffect(() => {
+    return () => {
+        loadOrders();
+      }
+    }, [])
+    const renderItem=({item})=>{
+        return <OrderItem task={item}/>
+    }
+
   return (
     <View>
-      <Text>OrderList</Text>
+      <FlatList
+        style={{width:"100%"}}  
+        data={pedidos}
+        renderItem={renderItem}
+        keyExtractor={(item)=>{item.id+""}}
+        />
+
     </View>
   )
 }
