e04f9218b310ddf545c02459cef9d39ff6af633e
Hector <hector141599@gmail.com>
2022-08-10 22:18:34 -0500

    Creando base d ela interfaz de pedidos

--- view/screens/Pedidos.js
+++ view/screens/Pedidos.js
@@ -1,15 +1,28 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet,TouchableOpacity } from 'react-native'
 import React from 'react'
+import { MaterialIcons,FontAwesome } from '@expo/vector-icons'
 
 const Pedidos = ({navigation}) => {
   return (
     <SafeAreaView>
-
-        <View>
-            <Text>Pedidos</Text>
+        <View style={style.header}>
+            <MaterialIcons name="arrow-back-ios" size={24} color="black" onPress={()=>{navigation.goBack()}}/>
+        </View>
+        <View style={{marginVertical:10,paddingHorizontal:20}}>
+            <Text style={{fontSize:24}}>Pedidos:</Text>
+            <View style={{width:"100%",backgroundColor:"#ccc",height:"100%",borderRadius:20,marginTop:20,padding:10}}>
+                <Text>Podras consultar tus pedidos en esta Pantalla</Text>
+            </View>
         </View>
     </SafeAreaView>
   )
 }
+const style= StyleSheet.create({
+    header:{paddingHorizontal:20,
+        marginTop:50,
+        flexDirection:"row",
+        justifyContent:"space-between",
+    }
+})
 
 export default Pedidos
