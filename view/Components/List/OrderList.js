24ab347c8a249d2b2964057f73d32904dc22f83a
Hector <hector141599@gmail.com>
2022-08-11 21:47:47 -0500

    Agregando solicitud al instate de cargar la pantalla de los pedidos

--- view/Components/List/OrderList.js
+++ view/Components/List/OrderList.js
@@ -1,30 +1,45 @@
-import { View, Text ,FlatList} from 'react-native'
+import { View, Text ,FlatList, RefreshControl} from 'react-native'
 import React,{useState,useEffect} from 'react'
 import OrderItem from './OrderItem'
 import { orders } from '../../../api'
-
+import { useIsFocused } from '@react-navigation/native'
 const OrderList = () => {
     const [pedidos, setPedidos] = useState([])
+    const [refresting, setRefresting] = useState(false);
+
+    const isFocused=useIsFocused();
 
     const loadOrders=async()=>{
         const data= await orders();
         setPedidos(data);
     }
     useEffect(() => {
-    return () => {
+      console.log(isFocused)
         loadOrders();
-      }
-    }, [])
+    }, [isFocused])
     const renderItem=({item})=>{
         return <OrderItem pedidos={item}/>
     }
 
+    const onRefresh= React.useCallback(async()=>{
+      setRefresting(true);
+      await loadTask();
+      setRefresting(false);
+  })
   return (
       <FlatList
         style={{width:"100%"}}
         data={pedidos}
         keyExtractor={(item)=>{item.id+" "}}
-        renderItem={renderItem}/>
+        renderItem={renderItem}
+        refreshControl={
+            <RefreshControl
+            refreshing={refresting}
+            colors={["#78e08f"]}
+            onRefresh={onRefresh}
+            progressBackgroundColor="#0a3d62"/>
+        }
+        />
   )
 }
 
