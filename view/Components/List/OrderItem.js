95a8bd5caf6eb89e765bf33bf7109a7ed77922ef
Hector <hector141599@gmail.com>
2022-08-11 19:32:48 -0500

    Solicitando todos los elementos de MySQL

--- view/Components/List/OrderItem.js
+++ view/Components/List/OrderItem.js
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const OrderItem = () => {
+const OrderItem = ({pedidos}) => {
   return (
-    <View>
-      <Text>OrderItem</Text>
+    <View style={{backgroundColor:"#f00",height:"100%",width:"100%"}}>
+    <Text>Servicio:</Text>
+      <Text>{pedidos.id}</Text>
+      <Text>{pedidos.codigop}</Text>
+      <Text>{pedidos.codcake}</Text>
+      <Text>{pedidos.idproducto}</Text>
+      <Text>{pedidos.cantidad}</Text>
+      <Text>{pedidos.total}</Text>
+      <Text>{pedidos.fechai}</Text>
+      <Text>{pedidos.fechaf}</Text>
+      <Text>{pedidos.username}</Text>
+      <Text>{pedidos.completo}</Text>
     </View>
   )
 }
